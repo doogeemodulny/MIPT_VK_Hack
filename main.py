654fa7db59d9b74d1b31282a88b2dfc9c13021ed
Ivan Chevychelov <114423547+Olonix@users.noreply.github.com>
2024-10-29 22:02:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import Augmentation as Aug  # На доработке
+import Augmentation as Aug
 import Dataset as Dt
 import MachineLearning as ML
 from sklearn.metrics import roc_auc_score, accuracy_score, precision_recall_fscore_support, classification_report
 
-USE_EXTRA_YANDEX_DATASET = False
+# USE_EXTRA_YANDEX_DATASET = False # теперь не используется
 
-train_df, val_df = Dt.CreateNecessaryDataset(USE_EXTRA_YANDEX_DATASET)  # False если не нужно подключать датасет Яндекса
+train_df, val_df = Dt.CreateNecessaryDataset()
 print('TrainDataFrame and ValidationDataFrame are loaded')
 train_df, val_df, test_df = Dt.SplitIntoTest(train_df, val_df)  # Если нужен тестовый датасет
 print('TrainDataFrame and ValidationDataFrame were split into TestDataFrame')
+
+# Аугментирование
+train_df = Aug.augment_relevant_answers_batch(train_df)
+
+# Добавление английского (+ undersampled) датасета
+train_df = Dt.AddMiraclEN(train_df)
+
 Dt.SaveDF(train_df, val_df, test_df)  # Дабы сохранять датафреймы
 trainingResults = ML.TrainingBertModel(train_df, val_df)
 train_texts, train_labels, valid_texts, valid_labels = trainingResults[0]
